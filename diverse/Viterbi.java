a879a2a4db0bcae30d9fd64bb0d60ddf9e1b4c97
swabhs <swabha.swayamdipta@gmail.com>
2012-10-22 13:47:46 -0400

    tests and fixes

--- diverse/Viterbi.java
+++ diverse/Viterbi.java
@@ -20,88 +20,65 @@ class Viterbi {
 	private Hypergraph h;
 	
 	/** Dynamic programming state saving variables */
-	private Map<Integer, Double> pi = new HashMap<Integer, Double>();
-	private Map<Integer, List<Hyperedge>> backPointers = new HashMap<Integer, List<Hyperedge>>();
-
+	List<Double> pi;
+	List<Hyperedge> backPointers;
+	
 	Viterbi(Hypergraph h){
 		this.h = h;
+		pi = new ArrayList<Double>(h.getVerticesCount());
+		backPointers = new ArrayList<Hyperedge>(h.getVerticesCount());
 	}
 	
-	/** Get the list of ids for the vertices which are not parents for any hyperedge */
-	List<Integer> getTerminals(Hypergraph h) {
-		List<Integer> terminals = new ArrayList<Integer>();
+	/** Initializes the weight of terminal nodes to 1.0 and the rest of the nodes to 0.0 */
+	List<Double> initialize() {
+		List<Integer> terminalIds = HypergraphUtils.getTerminals(h);
 		for (Vertex v : h.getVerticesList()) {
-			terminals.add(v.getId());
+			pi.add(v.getId() - 1, 0.0);
 		}
-		for (Hyperedge e : h.getEdgesList()) {
-			if (terminals.contains(e.getParentId())) {
-				terminals.remove(e.getParentId());
-			}
-		}
-		return terminals;
-	}
-	
-	/** 1*/
-	void initialize() {
-		List<Integer> terminalIds = getTerminals(h);
 		for(Integer id: terminalIds) {
-			pi.put(id, 1.0);		
+			pi.add(id - 1, 1.0);
 		}
-		for (Vertex v : h.getVerticesList()) {
-			if (!pi.containsKey(v.getId())) {
-				pi.put(v.getId(), 0.0);
-			}
-		}
-	}
-	
-	/** Construct a map that stores the list of incoming hyperedges for a parent vertex */
-	Map<Integer, List<Hyperedge>> generateIncomingMap() {
-		Map<Integer, List<Hyperedge>> inMap = new HashMap<Integer, List<Hyperedge>>();
-		for(Hyperedge e: h.getEdgesList()) {
-			List<Hyperedge> incoming;
-			if (inMap.containsKey(e.getParentId())) {
-				incoming = inMap.get(e.getParentId());
-			} else {
-				incoming = new ArrayList<Hyperedge>();
-			}
-			incoming.add(e);
-			inMap.put(e.getParentId(), incoming);
-		}
-		return inMap;
+		
+		return pi;
 	}
 	
 	/**
 	 * Run CKY and get a list of vertex ids which result in the highest probability structure
 	 */
 	List<Hyperedge> run() {
-		Map<Integer, List<Hyperedge>> inMap = generateIncomingMap();
-		List<Integer> vertices = toposort(getTerminals(h), h);
+		Map<Integer, List<Hyperedge>> inMap = HypergraphUtils.generateIncomingMap(h);
+		List<Integer> vertices = toposort(HypergraphUtils.getTerminals(h), h);
 		initialize();
 		for (Integer v: vertices) {
 			for (Hyperedge e : inMap.get(v)) {
 				double childProduct = 1.0;
-				List<Hyperedge> bestEdges;
+				//List<Hyperedge> bestEdges;
 				for (Integer i : e.getChildrenIdsList()) {
 					childProduct *= pi.get(i);
 				}
 				
 				if (pi.get(v) < childProduct * e.getWeight()) {
-					pi.put(v, childProduct * e.getWeight());
-					if (backPointers.containsKey(v)) {
+					pi.add(v, childProduct * e.getWeight());
+					/*if (backPointers.get(v) != null) {
 						bestEdges = backPointers.get(v);
 					} else {
 						bestEdges = new ArrayList<Hyperedge>();
 					}
 					bestEdges.add(e);
-					backPointers.put(v, bestEdges);
+					backPointers.add(v, bestEdges);*/
+					backPointers.add(v, e);
 				}
 			}
 		}
-		return backPointers.get(vertices.get(vertices.size() - 1));
+		return backPointers;//.get(vertices.get(vertices.size() - 1));
 	}
 	
+	/** Displays the 1-best parse */
 	void renderResult(List<Hyperedge> edges) {
-		
+		Hyperedge top = edges.get(h.getVerticesCount()-1);
+		while (top != null) {
+			System.out.println(h.getVertices(top.getParentId()).getName());
+		}
 	}
 	
 	/**
@@ -111,7 +88,7 @@ class Viterbi {
 	List<Integer> toposort(List<Integer> terminals, Hypergraph h) {
 		List<Integer> sorted = new ArrayList<Integer>();
 		Map<Integer, Boolean> visited = new HashMap<Integer, Boolean>();
-		Map<Integer, List<Hyperedge>> outMap = generateOutgoingMap();
+		Map<Integer, List<Hyperedge>> outMap = HypergraphUtils.generateOutgoingMap(h);
 		for(Vertex v : h.getVerticesList()) {
 			visited.put(v.getId(), false);
 		}
@@ -147,24 +124,4 @@ class Viterbi {
 		}
 	}
 	
-	/**
-	 * Runs in O(|V|+|E|).
-	 * @return
-	 */
-	Map<Integer, List<Hyperedge>> generateOutgoingMap() {
-		Map<Integer, List<Hyperedge>> outMap = new HashMap<Integer, List<Hyperedge>>();
-		for(Hyperedge e: h.getEdgesList()) {
-			List<Hyperedge> outgoing;
-			for (Integer childId : e.getChildrenIdsList()) {
-				if (outMap.containsKey(childId)) {
-					outgoing = outMap.get(e.getParentId());
-				} else {
-					outgoing = new ArrayList<Hyperedge>();
-				}
-				outgoing.add(e);
-				outMap.put(childId, outgoing);
-			}			
-		}
-		return outMap;
-	}
 }
--- diverse/ViterbiMain.java
+++ diverse/ViterbiMain.java
@@ -4,53 +4,11 @@
  */
 package shortestPath;
 
-import java.util.List;
-import java.util.ArrayList;
-
-import shortestPath.HypergraphProto.Hyperedge;
-import shortestPath.HypergraphProto.Hypergraph;
-import shortestPath.HypergraphProto.Vertex;
-
 public class ViterbiMain {
 
 	public static void main(String[] args) {
 		Viterbi v = new Viterbi(null);
 		v.run();
 	}
-	
-	static Hypergraph createHypergraph() {
-		List<Vertex> vertices = createVertices();
-		List<Hyperedge> edges = createEdges(vertices);
-		Hypergraph h = Hypergraph.newBuilder().addAllEdges(edges).addAllVertices(vertices).build();
-		return h;
-	}
-	
-	static List<Vertex> createVertices() {
-		List<Vertex> vertices = new ArrayList<Vertex>();
-		vertices.add(Vertex.newBuilder().setId(1).setName("the").build());
-		vertices.add(Vertex.newBuilder().setId(2).setName("man").build());
-		vertices.add(Vertex.newBuilder().setId(3).setName("laughed").build());
-		vertices.add(Vertex.newBuilder().setId(4).setName("D").build());
-		vertices.add(Vertex.newBuilder().setId(5).setName("N").build());
-		vertices.add(Vertex.newBuilder().setId(6).setName("V").build());
-		vertices.add(Vertex.newBuilder().setId(7).setName("NP").build());
-		vertices.add(Vertex.newBuilder().setId(8).setName("VP").build());
-		vertices.add(Vertex.newBuilder().setId(9).setName("S").build());
-		return vertices;
-	}
-	
-	static List<Hyperedge> createEdges(List<Vertex> vertices) {
-		List<Hyperedge> edges = new ArrayList<Hyperedge>();
-		edges.add(Hyperedge.newBuilder().setId(1).setParentId(6).setChildrenIds(0, 1).setWeight(1.0).build());
-		edges.add(Hyperedge.newBuilder().setId(2).setParentId(5).setChildrenIds(0, 2).setWeight(1.0).build());
-		edges.add(Hyperedge.newBuilder().setId(3).setParentId(4).setChildrenIds(0, 3).setWeight(1.0).build());
-		//edges.add(Hyperedge.newBuilder().setId(2).setParentId(5).setChildrenIds(0, 2).setWeight(1.0).build());
-		edges.add(Hyperedge.newBuilder().setId(5).setParentId(7).setChildrenIds(0, 5).setWeight(0.0).build());
-		edges.add(Hyperedge.newBuilder().setId(6).setParentId(8).setChildrenIds(0, 4).setWeight(1.0).build());
-		//edges.add(Hyperedge.newBuilder().setId(2).setParentId(5).setChildrenIds(0, 2).setWeight(1.0).build());
-		//edges.add(Hyperedge.newBuilder().setId(2).setParentId(5).setChildrenIds(0, 2).setWeight(1.0).build());
-		return edges;
-	}
-	
 	
 }
